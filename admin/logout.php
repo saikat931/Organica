34067c95f90a2ccc3a3da97a04146fecacca68f6
saikat931 <saikatsasmal931@gmail.com>
2025-08-01 23:52:59 +0530

    organica

--- admin/logout.php
+++ admin/logout.php
@@ -1,17 +1,4 @@
 <?php
-// session_start(); // Start the session to access session variables
-
-// // Destroy all session data
-// session_unset();  // Remove all session variables
-// session_destroy(); // Destroy the session
-
-// // Redirect to the login page
-// header('Location: login.php');
-// exit;
-?>
-
-
-<?php
 session_start(); // Start the session to access session variables
 
 // Destroy all session data
